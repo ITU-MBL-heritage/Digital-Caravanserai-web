dc41b240b74dc76fea63580877e5600355f2c5fe
nurerdemci <nur.erdemci1@gmail.com>
2020-05-25 15:23:12 +0300

    feat nur

---
+++ NUR/data/Pointsfromtable_2.js
@@ -0,0 +1,8 @@
+var json_Pointsfromtable_2 = {
+"type": "FeatureCollection",
+"name": "Pointsfromtable_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+
+]
+}
