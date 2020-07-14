be185011de6383cd3382f29c761bd8bd7141ab01
Ozgun Balaban <ozgunbalaban@yahoo.com>
2020-07-14 10:53:27 +0300

    Updates

---
+++ SEN/data/Turkhans_plus_2.js
@@ -0,0 +1,11 @@
+var json_Turkhans_plus_2 = {
+"type": "FeatureCollection",
+"name": "Turkhans_plus_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "1", "common_nam": "İbn Rabat", "alternativ": "Zara, Kervansaray-ı İşfahanı, Rıbat-ı Kemaleddin b. Rahat, Rıbat-ı Hace Ahmed, Rıbat-ı Pusar-i Rahat", "village": "Merkez", "district": "Zara", "province": "Sivas", "date": null, "references": "Bibi, İ. (1996). El Evamirü’l-Ala’iye Fi’l-Umuri’l-Ala’iye (Selçuk Name) I, Haz. Mürsel Öztürk, Kültür Bakanlığı Yayınları, Ankara, 182, Özergin, M. (1965). Anadoluda Selçuklu Kervansarayları. Tarih Dergisi, 15(20), 152, n. 52,", "Latitude": 37.75, "longitude": 39.69 }, "geometry": { "type": "Point", "coordinates": [ 37.75, 39.69 ] } },
+{ "type": "Feature", "properties": { "fid": "2", "common_nam": "Gedik Han", "alternativ": "Şarkışla Hanı", "village": "Merkez", "district": "Şarkışla", "province": "Sivas", "date": null, "references": "Özergin, M. (1965). Anadoluda Selçuklu Kervansarayları. Tarih Dergisi, 15(20), 150, n. 39,  Taeschner, F. (1924). Das anatolische Wegenetz nach osmanischen Quellen: Türkische Bibliothek, table 23,  Erdmann, K. (1961). Kervansaray Berlin, p.194.", "Latitude": 36.38, "longitude": 39.36 }, "geometry": { "type": "Point", "coordinates": [ 36.38, 39.36 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "common_nam": "Çubuk Han", "alternativ": null, "village": "Çubuk", "district": "Merkez", "province": "Sivas", "date": null, "references": "Eravşar O.(2017). Witnesses of the way: the caravanserais of Seljuk Anatolia. Türkiye Cumhuriyeti Cumhurbaşkanlığı, 34-45.", "Latitude": 36.5, "longitude": 39.62 }, "geometry": { "type": "Point", "coordinates": [ 36.5, 39.62 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "common_nam": "Zurun Han", "alternativ": "Köprübaşı Hanı", "village": "Akyurt", "district": "Tercan", "province": "Erzincan", "date": null, "references": "ÇEKUL", "Latitude": 40.261663, "longitude": 39.735925 }, "geometry": { "type": "Point", "coordinates": [ 40.261663, 39.735925 ] } }
+]
+}
